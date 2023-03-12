262d5c8bac5ce7330c6eb92ba369a927cae79697
Zsolti0819 <kiss.zsolt0819@gmail.com>
2023-03-12 13:59:21 +0100

    Modifying paths in Constants

--- src/main/java/com/github/Constants.java
+++ src/main/java/com/github/Constants.java
@@ -2,11 +2,16 @@ package com.github;
 
 import lombok.experimental.UtilityClass;
 
+import java.io.File;
+import java.nio.file.Path;
+
 @UtilityClass
 public class Constants {
-
-    public static final String SQLITE_CONNECTION_STRING = "jdbc:sqlite:C:\\Users\\f7ifsut\\Downloads\\driver-comparison\\src\\main\\resources\\myDatabase.db";
-    public static final String JFSQL_CONNECTION_STRING = "jdbc:jfsql:C:\\Users\\f7ifsut\\Downloads\\driver-comparison\\src\\main\\resources\\myDatabase";
-    public static final String SCRIPTS_FOLDER = "C:\\Users\\f7ifsut\\Downloads\\driver-comparison\\src\\main\\resources\\scripts";
+    private static final String BASE_PATH = System.getProperty("user.dir");
+    public static final Path RESOURCES_PATH = Path.of(
+            BASE_PATH + File.separator + "src" + File.separator + "main" + File.separator + "resources");
+    public static final String SQLITE_CONNECTION_STRING = "jdbc:sqlite:" + RESOURCES_PATH + File.separator + "myDatabase.db";
+    public static final String JFSQL_CONNECTION_STRING = "jdbc:jfsql:" + RESOURCES_PATH + File.separator + "myDatabase";
+    public static final String SCRIPTS_FOLDER = RESOURCES_PATH + File.separator + "scripts";
 
 }
